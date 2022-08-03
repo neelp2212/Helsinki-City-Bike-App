2b3a461da5c183628406a3b422b7c598fc6a6f25
Neel Patel <neel.patel@metropolia.fi>
2022-08-03 15:00:49 +0300

    - Import GoogleMapReact for the map - Using Css media query hook for react. It renders components on whether the query matches or not. - Using onChange event to change the components.

--- src/Components/Map/map.jsx
+++ src/Components/Map/map.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from'@material-ui/icons';
+
+import useStyles from './styles.js'
 
 const Map = () => {
+
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat:0, lon:0 }
     return(
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: '' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50,]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     )
 }
 
